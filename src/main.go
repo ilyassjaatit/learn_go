31606a99699983c2b6d5e77e3c660d5ab549f458
Ilyass Jaatit <ilyassjaatit@gmail.com>
2021-12-06 16:20:14 +0100

    var and constants

--- src/main.go
+++ src/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//Constants
+	const pi float64 = 3.141592653589793
+	const euler = 2.71828
+	const hello_words string = "hello world"
+	fmt.Println("pi", pi)
+	fmt.Println("Euler", euler)
+	fmt.Println(hello_words)
+
+	//Sariables
+	var one int = 1
+	var two int
+	three := 3
+	two = 2
+
+	fmt.Println("one", one)
+	fmt.Println("two", two)
+	fmt.Println("three", three)
 }
